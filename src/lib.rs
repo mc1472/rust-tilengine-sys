b5f7355e04623b7967c577f7145ba9e94a7689e3
mc1472 <mc1472@protonmail.com>
2019-09-16 21:32:53 -0500

    Added missing functions and ttpes.
    I added missing funcion and types that where missed by bindgen.

--- src/lib.rs
+++ src/lib.rs
@@ -4,4 +4,33 @@
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
+#[repr(C)]
+pub struct TLN_SpriteState {
+    x : isize,
+    y : isize, 
+    w : isize,
+    h : isize,
+    flags : TLN_TileFlags,
+    palatte : TLN_Palette,
+    spriteset : TLN_Spriteset,
+    index : isize,
+    enabled : bool,
+    collision : bool
+}
+
+#[repr(C)]
+pub enum TLN_LogLevel {
+    TLN_LOG_NONE,
+    TLN_LOG_ERRORS,
+    TLN_LOG_VERBOSE,
+}
+
+extern {
+    pub fn TLN_GetSpriteStates(nsprite: isize, state : *mut TLN_SpriteState) -> isize;
+    pub fn TLN_SetLogLevel(log_level : TLN_LogLevel);
+}
+
+
+
+
 
